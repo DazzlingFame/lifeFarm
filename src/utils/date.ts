302e1ce8bfa46c74836d55e492915baa6ff17f22
DazzlingFame <narsel1995@mail.ru>
2021-06-20 18:17:01 +0300

    android date formatting fix

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,10 +1,18 @@
 type DateParser = (date: Date) => string;
-export const parseDate: DateParser = (date) =>
-  date.toLocaleString('ru', {
+export const parseDate: DateParser = (date) => {
+  console.log(
+    date.toLocaleString('ru', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    }),
+  );
+  return date.toLocaleString('ru', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
   });
+};
 
 type DateTimeParser = (dateTime: number) => string;
 export const parseDateTime: DateTimeParser = (dateTime) =>
